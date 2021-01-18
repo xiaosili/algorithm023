06e21ccabf241700d869bdd663da0297eec013cf
lixiaosi <3099668942@qq.com>
2021-01-18 21:38:27 +0800

    二叉树前序遍历新增迭代方法

--- Week_03/preorderTraversal.js
+++ Week_03/preorderTraversal.js
@@ -12,3 +12,18 @@ var preorderTraversal = function (root) {
   preorder(root);
   return res;
 };
+// 迭代
+var preorderTraversal = function (root) {
+  let res = [];
+  let stk = [];
+  while (root || stk.length) {
+    while (root) {
+      res.push(root.val);
+      stk.push(root);
+      root = root.left;
+    }
+    root = stk.pop();
+    root = root.right;
+  }
+  return res;
+};
