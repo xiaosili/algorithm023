9a7d6a913dbedaf4d2dc024c71ca8ffaad88e2e4
lixiaosi <3099668942@qq.com>
2021-02-01 10:53:03 +0800

    添加边界条件判断

--- Week_01/threeSum.js
+++ Week_01/threeSum.js
@@ -3,17 +3,21 @@
  * @return {number[][]}
  */
 var threeSum = function (nums) {
+  let len = nums.length;
+  if (len < 3) return [];
+  nums.sort((a, b) => a - b);
+  if (nums[0] > 0 || nums[len - 1] < 0) return [];
   // 1,排序
   // 2，验证2数和，动态规划
   nums.sort((a, b) => a - b);
   let arr = [];
-  for (let k = 0; k < nums.length - 2; k++) {
+  for (let k = 0; k < len - 2; k++) {
     if (nums[k] > 0) {
       break;
     }
     if (k > 0 && nums[k] === nums[k - 1]) continue;
     let i = k + 1,
-      j = nums.length - 1;
+      j = len - 1;
     while (i < j) {
       let sum = nums[k] + nums[i] + nums[j];
       if (sum < 0) {
