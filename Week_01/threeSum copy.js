78d37f0ca6775cc1ca0ffe7e30d13bb253e4312d
lixiaosi <3099668942@qq.com>
2021-02-01 10:53:31 +0800

    新增暴力法和参考题解

--- Week_01/threeSum copy.js
+++ Week_01/threeSum copy.js
@@ -22,3 +22,54 @@
   };
   threeSum(nums);
 }
+
+// 暴力2---超出时间限制
+{
+  var threeSum = function (nums) {
+    let len = nums.length;
+    if (len < 3) return [];
+    nums.sort((a, b) => a - b);
+    if (nums[0] > 0) return [];
+    if (nums[len - 1] < 0) return [];
+    let map = new Map();
+    for (i = 0; i < len; i++) {
+      for (let j = i + 1; j < len - 2; j++) {
+        for (let z = j + 1; z < len - 1; z++) {
+          if (nums[i] + nums[j] + nums[z] === 0) {
+            let key = [nums[i], nums[j], nums[z]].toString();
+            if (!map.has(key)) {
+              map.set(key, [nums[i], nums[j], nums[z]]);
+            }
+          }
+        }
+      }
+    }
+    return Array.from(map.values());
+  };
+}
+
+// 参考题解
+var threeSum = function (nums) {
+  let ans = [];
+  const len = nums.length;
+  if (nums == null || len < 3) return ans;
+  nums.sort((a, b) => a - b); // 排序
+  for (let i = 0; i < len; i++) {
+    if (nums[i] > 0) break; // 如果当前数字大于0，则三数之和一定大于0，所以结束循环
+    if (i > 0 && nums[i] == nums[i - 1]) continue; // 去重
+    let L = i + 1;
+    let R = len - 1;
+    while (L < R) {
+      const sum = nums[i] + nums[L] + nums[R];
+      if (sum == 0) {
+        ans.push([nums[i], nums[L], nums[R]]);
+        while (L < R && nums[L] == nums[L + 1]) L++; // 去重
+        while (L < R && nums[R] == nums[R - 1]) R--; // 去重
+        L++;
+        R--;
+      } else if (sum < 0) L++;
+      else if (sum > 0) R--;
+    }
+  }
+  return ans;
+};
