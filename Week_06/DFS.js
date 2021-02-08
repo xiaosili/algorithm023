552edcd5c13051b5ef21f74e621a774021556e89
lixiaosi <3099668942@qq.com>
2021-02-09 07:41:18 +0800

    dfs模版

---
+++ Week_06/DFS.js
@@ -0,0 +1,16 @@
+const visited = new Set();
+const dfs = (node) => {
+  if (visited.has(node)) return;
+  visited.add(node);
+  dfs(node.left);
+  dfs(node.right);
+};
+
+const visited = new Set();
+const dfs = (node, visited) => {
+  if (visited.has(node)) return;
+  visited.add(node);
+  for (next_node of node.children()) {
+    if (!visited.has(next_node)) dfs(next_node, visited);
+  }
+};
